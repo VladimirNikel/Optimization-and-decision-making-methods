47829b46df09fa9d64f1bcb1200c555c0567f5e6
VladimirNikel <vova_niemisto@mail.ru>
2021-03-29 18:44:20 +0300

    произведен код-ревью по второму заданию

--- laba2/gen_csv.py
+++ laba2/gen_csv.py
@@ -1,83 +1,91 @@
 from faker import Faker
 import random
 import os
+import csv
 
-list_joke_string 	= [	'Это не число. Попробуй снова:',
-						'Ну вот опять: ну не число это. Понимаешь? НЕ ЧИСЛО! Давай по новой:',
-						'Никогда такого не было, и вот опять... Ты смеешься?',
-						'Ну всё! Крайний шаг: перевожу на китайский!',
-						'Выйди и зайди нормально!']
-count_joke_moment 	= 5
-joke 			= False
-list_departments 	= []
-file_name 		= 'test.csv'
-new_file_name 		= 'test-old.csv'
-finaly_word 		= "="*50 + "\nДо встречи, бро!\nNikel, 2021"
-separator = ';'
+""" It is a decoration of the program. """
+joke = False
+
 
 def get_count_departments() -> int:
-	"""функция получения количества отделов
-	нужна, чтобы не позволить пользователю с очумелыми ручками сломать программу"""
+    """Function for getting the number of departments from the user.
+    Errors from entering non-numbers are handled, and the number is checked for matching the range from 1 to 9."""
 
-	global list_joke_string
-	global count_joke_moment
-	global joke
+    joke_string = [
+        'Это не число. Попробуй снова:',
+        'Ну вот опять: ну не число это. Понимаешь? НЕ ЧИСЛО! Давай по новой:',
+        'Никогда такого не было, и вот опять... Ты смеешься?',
+        'Ну всё! Крайний шаг: перевожу на китайский!',
+        'Выйди и зайди нормально!']
+    count_joke_moment = 5
+    global joke
 
-	count_departments = 0
-	"""далее код проверок написан в личных интересах (чисто поржать)"""
-	while True:
-		try:
-			count_departments = int(input('Введите количество отделов в фирме: '))
-			if (count_departments > 0 and count_departments < 10):
-				return count_departments
-			else:
-				print("Давай это число будет от 1 до 9... А то что это за фирма такая?)\n")
-		except ValueError:
-			print("{}\n".format(list_joke_string.pop(0)))
-			if count_joke_moment == 2:
-				joke = True
-			elif count_joke_moment == 1:
-				exit(0)
-			count_joke_moment -= 1
+    """далее код проверок написан в личных интересах (чисто поржать)"""
+    while True:
+        try:
+            my_count_departments = int(input('Введите количество отделов в фирме: '))
+            if 0 < my_count_departments < 10:
+                return my_count_departments
+            else:
+                print("Давай это число будет от 1 до 9... А то что это за фирма такая?)\n")
 
-"""основной скрипт программы"""
-try:
-	print("="*50,"\nГенератор CSV-файлов 3000.\nПредназначен для генерации CSV-файлов, использующихся для тестирования.\nРазработчик: Nikel, М30-117М-20\n2021 Moscow.\n"+"="*50, "\n")
+        # catching the error of entering a non-number
+        except ValueError:
+            print("{}\n".format(joke_string.pop(0)))
+            if count_joke_moment == 2:
+                joke = True
+            elif count_joke_moment == 1:
+                exit(0)
+            count_joke_moment -= 1
 
-	count_departments = get_count_departments()
-	fake = Faker('ru_RU')					#инициализация генератора
-	if joke:
-		fake = Faker('zh_CN')				#инициализация ржачного генератора
 
-	for i in range(count_departments):
-		list_departments.append(fake.bs())
-	
-	"""основная генерация данных"""
-	if os.path.exists(file_name):
-		os.rename(file_name, new_file_name)
-		print("\nДля сохранения предыдущих результатов, имеющийся файл {} был переименован в {}.".format(file_name, new_file_name))
+if __name__ == "__main__":
+    start_message = "=" * 50 + """
+Генератор CSV-файлов 3000.
+Предназначен для генерации CSV-файлов, использующихся для тестирования.
+Разработчик: Nikel, М30-117М-20\n2021 Moscow.\n""" + "="*50 + "\n"
+    finally_word = "=" * 50 + "\nДо встречи, бро!\nNikel, 2021"
+    list_departments = []
+    name_file = 'test.csv'
+    new_name_file = 'test-old.csv'
+    separator = ';'
 
-	file = open(file_name,'w')	
-	if file:								#проверка на возможность создать файл
-		#tmp = ("ФИО", "Должность", "Подразделение", "Квартальная оценка", "Зарплата")
-		#file.write(separator.join(tmp)+'\n')
-		count_record_in_new_file = random.randint(100,140)
-		for i in range(count_record_in_new_file):
-			list_param = (
-				fake.name(),											#ФИО
-				fake.job(),												#должность
-				random.choice(list_departments),						#принадлежность к одному из подразделений
-				str(fake.random_int(min=1, max=5)),						#квартальная оценка
-				str(fake.random_int(min=25000, max=150000, step=500))	#текущая зарплата
-			)
-			file.write(separator.join(list_param)+'\n')
+    try:
+        fake = Faker('ru_RU')					        # initializing the Faker generator
+        if joke:
+            fake = Faker('zh_CN')				        # initializing the joke Faker generator
+        print(start_message)                            # print start_message - it's menu
+        count_departments = get_count_departments()     # request for the count of departments
 
-		file.close()
-		print("\nУспешно создан файл {} с количеством записей: {}\n{}".format(file_name, count_record_in_new_file, finaly_word))
-		exit(0)
+        # filling in the list of departments with data from the Faker
+        for i in range(count_departments):
+            list_departments.append(fake.bs())
 
-	print("Не удалось открыть/создать файл. Программа завершается." + finaly_word)
-	exit(0)
-	
-except KeyboardInterrupt:
-	print("\033A\n\n" + finaly_word)
+        # checking for a file with the specified name
+        if os.path.exists(name_file):
+            os.rename(name_file, new_name_file)
+            print("\nДля сохранения предыдущих результатов, имеющийся файл {} был переименован в {}.".format(
+                name_file,
+                new_name_file))
+
+        with open(name_file, 'w', newline='') as open_file:
+            count_record_in_file = random.randint(100, 140)
+            for i in range(count_record_in_file):
+                set_param = (
+                    fake.name(),											# full name
+                    fake.job(),												# job
+                    random.choice(list_departments),						# one of departments
+                    str(fake.random_int(min=1, max=5)),						# quarterly assessment
+                    str(fake.random_int(min=25000, max=150000, step=500))   # current salary
+                )
+                writer = csv.writer(open_file, delimiter=separator)
+                writer.writerow(set_param)
+
+            print(f"\nУспешно создан файл {name_file} с количеством записей: {count_record_in_file}\n{finally_word}")
+            exit(0)
+
+        print("Не удалось открыть/создать файл. Программа завершается." + finally_word)
+        exit(0)
+
+    except KeyboardInterrupt:
+        print("\033A\n\n" + finally_word)
--- laba2/parser_csv.py
+++ laba2/parser_csv.py
@@ -1,149 +1,191 @@
 import os
 import numpy
-
-input_file_name 	= 'test.csv'
-finaly_word 		= "="*50 + "\nДо встречи, бро!\nNikel, 2021"
-menu 			= """==================Меню=действий===================
-Меню действий:
-	1. Вывести все отделы
-	2. Вывести сводный отчёт по отделам
-	3. Сохранить сводный отчёт в CSV-файл
-	4. Изменить название/путь входного файла
-	5. Выйти из программы\n"""+"="*50
-
-output_data = ""
-separator = ';'
-
-"""функция получения списка из значений "ячеек" по строкам из файла"""
-def get_str_from_file(name_file: str) -> list:
-	str_from_file = []
-	with open(name_file, 'r') as file:
-		for n, line in enumerate(file, 1):
-			line = line.rstrip('\n')
-			str_from_file.append(line.split(separator))
-	return str_from_file
-
-"""функция получения списка только уникальных значений из списка отделов"""
-def get_unique_list_department(name_file: str) -> list:
-	list_department = []
-	for i in get_str_from_file(name_file):
-		list_department.append(i[2])
-	return set(list_department)
-
-def get_list_data_department(name_file: str, department: str) -> list:
-	out_list = []
-	with open(name_file, 'r') as file:
-		for n, line in enumerate(file, 1):
-			line = line.rstrip('\n')
-			tmp = line.split(separator)
-			if tmp[2] == department:
-				out_list.append([tmp[0], tmp[3], tmp[4]])
-	return out_list
-
-"""функция получения ответа от пользователя"""
-def asking_questions(question: str, answer1: str, answer2: str) -> bool:
-	global number_question
-	print(question)
-	options = {answer1: True, answer2: False}
-	while option not in options:
-		print('\tВыберите: {}/{}'.format(*options))
-		option = input("\tВаш ответ: ")
-	if options[option]:
-		return True
-	return False
+import csv
 
 
-"""вызываемый скрипт для вывода списка отделов, которые перечислены во входном файле"""
-def print_departments():
-	print("***************Вывод*списка*отделов***************")
-	unique_list_department = get_unique_list_department(input_file_name)
-	print("\nВ файле {} были перечислены следующие отделы:".format(input_file_name))
-	for n, item in enumerate(unique_list_department, 1):
-		print("\t{}) {}".format(n,item))
+def get_str_from_file(name_file: str, delimiter=';') -> list:
+    """Function for getting a list of "cell" values by lines from a file.
+    The name_file variable is required. It is used to pass the full path of the file in question.
+    Output value: list of data by row."""
+    str_from_file = []
+    with open(name_file, 'r') as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        for row in reader:
+            str_from_file.append(row)
+    return str_from_file
 
-"""вызываемый скрипт для вывода сводного отчета по отделам, которые перечислены во входном файле"""
-def print_report():
-	global output_data
-	print("*********Вывод*сводного*отчета*по*отделам*********\n")
-	output_data += (f"Отчёт по файлу {input_file_name}\n")
-	tmp = (f"Название отдела", "Численность", "min-max зарплата", "Средняя зарплата")
-	output_data += (separator.join(tmp)+'\n')
-	for n, department in enumerate(get_unique_list_department(input_file_name), 1):
-		count_worker = 0
-		list_salary = []
 
-		for item_in_department in get_list_data_department(input_file_name, department):
-			count_worker += 1
-			list_salary.append(int(item_in_department[2]))
+def get_unique_list_department(name_file: str, delimiter=';') -> list:
+    """Function for getting a list of only unique values from a list of departments.
+    The name_file variable is required and is used to pass the path of the file under investigation.
+    The delimiter variable is optional and stores the delimiter character used in the file.
+    Output value: a unique list of departments that were listed in the file."""
+    list_department = []
+    for item in get_str_from_file(name_file, delimiter):
+        list_department.append(item[2])
+    return list(set(list_department))
 
-		fork_salary = "{}-{}".format(min(list_salary), max(list_salary))
-		average_salary = str(int(numpy.average(list_salary)))
 
-		print("Название {} отдела: {}\n\tЧисленность: {}\n\tДиапазон зарплат: {}\n\tСредняя зарплата: {}\n".format(
-			n, 					#номер отдела
-			department, 			#название
-			count_worker,			#численность
-			fork_salary,			#min-max
-			average_salary			#средняя зарплата
-			)
-		)
-		tmp = (department, str(count_worker), fork_salary, average_salary)
-		output_data += (separator.join(tmp)+'\n')
+def get_list_data_department(name_file: str, department: str, delimiter=';') -> list:
+    """Function for getting a list of values (full name, quarterly estimate, and current salary) of employees
+    for a specific department from the input file.
+    The name_file variable is required and is used to pass the path to the file under investigation.
+    The department variable is required and is used to pass a string value for the department name.
+    The delimiter variable is optional and stores the delimiter character used in the file.
+    Output value: a list of values (full name, quarterly estimate, and current salary) of the employee
+    for a specific department from the input file."""
+    out_list = []
+    input_date = get_str_from_file(name_file, delimiter)
+    for line in input_date:
+        if line[2] == department:
+            out_list.append([line[0], line[3], line[4]])    # full name, quarterly estimate, and current salary
+    return out_list
 
-	
-"""функция сохранения сводного отчета в CSV-файл"""
-def save_report_in_csv():
-	print("*******Сохранение*сводного*отчёта*в*CSV-файл******")
-	if output_data == "":
-		print("\n\t\t!!!ВНИМАНИЕ!!!\nВы не сформировывали сводный отчёт. Операция прервана.\n")
-		return
-	while True:
-		name_output_file = input("\nВведите название выходного файла: ")
-		if name_output_file[-4:] != ".csv":
-			name_output_file += ".csv"
-		if not os.path.exists(name_output_file):
-			break
-		if asking_questions("\n\t\t!!!ВНИМАНИЕ!!!\nТакой файл уже существует! Вы хотите перезаписать файл?", "да", "нет"):
-			break
-		print("\tТогда необходимо повторить операцию.")
-	print("\nИмеющийся отчет записан в файл: {}".format(name_output_file))
-	file = open(name_output_file, 'w')
-	file.write(output_data)
-	file.close()
 
-"""функция изменения пути входного/анализируемого файла"""
-def change_file_name():
-	global input_file_name
-	tmp = ""
-	print("************Смена*пути*входного*файла*************")
-	while True:
-		tmp = input("Введите полный путь до csv-файла: ")
-		if os.path.exists(tmp):
-			input_file_name = tmp
-			break
-		print("\n\t\t!!!ВНИМАНИЕ!!!\nТакого файла не существует. Проверьте правильность ввода.\n")
+def asking_questions(question: str, positive_answer: str, negative_answer: str) -> bool:
+    """The function of asking the transmitted context-question.
+    The question variable must contain the question string.
+    The positive_answer variable must contain the string value of the positive response that will be offered to the user.
+    The negative_answer variable must contain the value of the negative response that will be offered to the user.
+    Returns True if a positive response option is selected, and False if a negative response option is selected."""
+    print(question)
+    option = ''
+    options = {positive_answer: True, negative_answer: False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input("Ваш ответ: ")
+    return options[option]
 
-try:
-	print("="*50,"\nПарсер CSV-файлов 3000.\nПредназначен для анализа CSV-файлов.\nРазработчик: Nikel, М30-117М-20\n2021 Moscow.\n"+"="*50, "\n")
-	action_code = 0
-	while True:	
-		try:
-			print("Для анализа используется файл: {}\n\n{}".format(input_file_name, menu))
-			action_code = int(input("Ваш выбор: "))
-			if action_code == 1:
-				print_departments()
-			elif action_code == 2:
-				print_report()
-			elif action_code == 3:
-				save_report_in_csv()
-			elif action_code == 4:
-				change_file_name()
-			elif action_code == 5:
-				print("\n{}".format(finaly_word))
-				break
-			print("*"*50,"\n")
-		except ValueError:
-			print("Вы ввели не число. Попробуйте снова.\n")
 
-except KeyboardInterrupt:
-	print("\033A\n\n" + finaly_word)
+def get_departments(name_file: str, delimiter=';') -> str:
+    """The called script for displaying the list of departments that are listed in the input file.
+    The name_file variable is required and is used to pass the path to the file under investigation.
+    The delimiter variable is optional and stores the delimiter character used in the file.
+    A string value is returned - the generated response of the function."""
+    out_data = "***************Вывод*списка*отделов***************"
+    unique_list_department = get_unique_list_department(name_file, delimiter)
+    out_data += "\nВ файле {} были перечислены следующие отделы:".format(name_file)
+    for n, item in enumerate(unique_list_department, 1):
+        out_data += "\n\t{}) {}".format(n, item)
+    return out_data
+
+
+def get_report(name_file: str, delimiter=';') -> str:
+    """The called script for displaying a summary report on the departments that are listed in the input file.
+    The name_file variable is required and is used to pass the path to the file under investigation.
+    The delimiter variable is optional and stores the delimiter character used in the file.
+    The output is a generated report in the form of str."""
+
+    print("*********Вывод*сводного*отчета*по*отделам*********\n")
+    out_data = f"Отчёт по файлу {name_file}\n"
+    tmp = (f"Название отдела", "Численность", "min-max зарплата", "Средняя зарплата")
+    out_data += (separator.join(tmp)+'\n')
+
+    # Crawling the entire file by department
+    for n, department in enumerate(get_unique_list_department(name_file, delimiter), 1):
+        count_worker = 0
+        list_salary = []
+
+        """ Calculating the required data (number of employees, minimum-maximum salary, and average salary) within 
+        a single department."""
+        for item_in_department in get_list_data_department(name_file, department):
+            count_worker += 1
+            list_salary.append(int(item_in_department[2]))
+
+        fork_salary = "{}-{}".format(min(list_salary), max(list_salary))
+        average_salary = str(int(numpy.average(list_salary)))
+
+        print("Название {} отдела: {}\n\tЧисленность: {}\n\tДиапазон зарплат: {}\n\tСредняя зарплата: {}\n".format(
+            n,                      # номер отдела
+            department,             # название
+            count_worker,           # численность
+            fork_salary,            # min-max
+            average_salary          # средняя зарплата
+        )
+        )
+        tmp = (department, str(count_worker), fork_salary, average_salary)
+        out_data += (separator.join(tmp)+'\n')
+    return out_data
+
+
+def get_name_output_file() -> str:
+    """Function for getting the output file name from the user."""
+    question = "\n\t\t!!!ВНИМАНИЕ!!!\nТакой файл уже существует! Вы хотите перезаписать файл?"
+    while True:
+        name_output_file = input("\nВведите название выходного файла: ")
+        if name_output_file[-4:] != ".csv":
+            name_output_file += ".csv"
+        if not os.path.exists(name_output_file):
+            break               # there is no file at the specified path (this is a new file)
+        if asking_questions(question, "да", "нет"):
+            break               # a file exists at the specified path, but it can be overwritten
+        print("\tТогда необходимо повторить операцию.")
+    return name_output_file
+
+
+def save_report_in_csv(name_output_file: str, data_report: str) -> str:
+    """Function for saving a summary report to a CSV file."""
+    out_data = "\n*******Сохранение*сводного*отчёта*в*CSV-файл******"
+    if data_report == "":
+        out_data += "\n\t\t!!!ВНИМАНИЕ!!!\nВы не сформировывали сводный отчёт. Операция прервана.\n"
+        return out_data
+    with open(name_output_file, 'w', newline='') as open_file:
+        open_file.write(data_report)
+    out_data += "\n\nИмеющийся отчет записан в файл: {}\n".format(name_output_file)
+    return out_data
+
+
+def change_file_name() -> str:
+    """Function for changing the path of the input / analyzed file."""
+    print("************Смена*пути*входного*файла*************")
+    while True:
+        tmp = input("Введите полный путь до csv-файла: ")
+        if tmp[-4:] == ".csv":
+            if os.path.exists(tmp):
+                return tmp
+            print("\n\t\t!!!ВНИМАНИЕ!!!\nТакого файла не существует. Проверьте правильность ввода.\n")
+        print("\n\t\t!!!ВНИМАНИЕ!!!\nЭто не CSV-файл. Проверьте правильность ввода.\n")
+
+
+if __name__ == "__main__":
+    start_message = "=" * 50 + """
+Парсер CSV-файлов 3000.
+Предназначен для анализа CSV-файлов.
+Разработчик: Nikel, М30-117М-20
+2021 Moscow.\n""" + "=" * 50 + "\n"
+    menu = """==================Меню=действий===================
+    Меню действий:
+    1. Вывести все отделы
+    2. Вывести сводный отчёт по отделам
+    3. Сохранить сводный отчёт в CSV-файл
+    4. Изменить название/путь входного файла
+    5. Выйти из программы\n""" + "=" * 50
+    finally_word = "=" * 50 + "\nДо встречи, бро!\nNikel, 2021"
+    input_file_name = 'test.csv'
+    report = ""
+    separator = ';'
+
+    try:
+        print(start_message)
+        action_code = 0
+        while True:
+            try:
+                print("Для анализа используется файл: {}\n\n{}".format(input_file_name, menu))
+                action_code = int(input("Ваш выбор: "))
+                if action_code == 1:
+                    print(get_departments(input_file_name, separator))
+                elif action_code == 2:
+                    report = get_report(input_file_name, separator)
+                elif action_code == 3:
+                    output_file = get_name_output_file()
+                    print(save_report_in_csv(output_file, report))
+                elif action_code == 4:
+                    input_file_name = change_file_name()
+                elif action_code == 5:
+                    print("\n{}".format(finally_word))
+                    break
+                print("*"*50, "\n")
+            except ValueError:
+                print("Вы ввели не число. Попробуйте снова.\n")
+    except KeyboardInterrupt:
+        print("\033A\n\n" + finally_word)
