6e8c901d434d9da2cc4425e10f117212a8fceefd
VladimirNikel <vova_niemisto@mail.ru>
2021-05-10 19:33:33 +0300

    fix test_whatIsYearNow laba3

--- laba3/whatIsYearNow/testWhatIsYearNow.py
+++ laba3/whatIsYearNow/testWhatIsYearNow.py
@@ -1,60 +1,48 @@
 import pytest
 import what_is_year_now
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 
-@patch('urllib.request.urlopen')
-def test_format_v1(urlopen):
-    mock = MagicMock()
-    mock.read.return_value = '{"$id":"1",\
-        "currentDateTime":"2021-04-09T19:29Z",\
-        "utcOffset":"00:00:00",\
-        "isDayLightSavingsTime":false,\
-        "dayOfTheWeek":"Friday",\
-        "timeZoneName":"UTC",\
-        "currentFileTime":132624701535543575,\
-        "ordinalDate":"2021-99",\
-        "serviceResponse":null}'
-    mock.__enter__.return_value = mock
-    urlopen.return_value = mock
-
-    year = what_is_year_now.what_is_year_now()
-    assert year == 2021
+def test_format_v1():
+    json_return_value = {
+        "$id": "1",
+        "currentDateTime": "2022-04-09T19:29Z",
+        "utcOffset": "00:00:00",
+        "serviceResponse": "null"
+    }
+    with patch('json.load', return_value=json_return_value):
+        year = what_is_year_now.what_is_year_now()
+        assert year == 2022
 
 
-@patch('urllib.request.urlopen')
-def test_format_v2(urlopen):
-    mock = MagicMock()
-    mock.read.return_value = '{"$id":"1",\
-        "currentDateTime":"09.04.2021T19:29Z",\
-        "utcOffset":"00:00:00",\
-        "isDayLightSavingsTime":false,\
-        "dayOfTheWeek":"Friday",\
-        "timeZoneName":"UTC",\
-        "currentFileTime":132624701535543575,\
-        "ordinalDate":"2021-99",\
-        "serviceResponse":null}'
-    mock.__enter__.return_value = mock
-    urlopen.return_value = mock
-
-    year = what_is_year_now.what_is_year_now()
-    assert year == 2021
+def test_format_v2():
+    json_return_value = {
+        "$id": "1",
+        "currentDateTime": "09.04.2023T19:29Z",
+        "utcOffset": "00:00:00",
+        "isDayLightSavingsTime": "false",
+        "dayOfTheWeek": "Friday",
+        "timeZoneName": "UTC",
+        "currentFileTime": 132624701535543575,
+        "ordinalDate": "2023-99",
+        "serviceResponse": "null"
+    }
+    with patch('json.load', return_value=json_return_value):
+        year = what_is_year_now.what_is_year_now()
+        assert year == 2023
 
 
-@patch('urllib.request.urlopen')
-def test_exception_format(urlopen):
-    mock = MagicMock()
-    mock.read.return_value = '{"$id":"1",\
-        "currentDateTime":"2021/04/09T19:29Z",\
-        "utcOffset":"00:00:00",\
-        "isDayLightSavingsTime":false,\
-        "dayOfTheWeek":"Friday",\
-        "timeZoneName":"UTC",\
-        "currentFileTime":132624701535543575,\
-        "ordinalDate":"2021-99",\
-        "serviceResponse":null}'
-    mock.__enter__.return_value = mock
-    urlopen.return_value = mock
-
-    with pytest.raises(Exception):
+def test_exception_format():
+    json_return_value = {
+        "$id": "1",
+        "currentDateTime": "2024/04/09T19:29Z",
+        "utcOffset": "00:00:00",
+        "isDayLightSavingsTime": "false",
+        "dayOfTheWeek": "Friday",
+        "timeZoneName": "UTC",
+        "currentFileTime": 132624701535543575,
+        "ordinalDate": "2021-99",
+        "serviceResponse": "null"
+    }
+    with patch('json.load', return_value=json_return_value), pytest.raises(Exception):
         what_is_year_now.what_is_year_now()
